9bedb414c15e921d2daf7686a482f1fc98ee7443
unknown <634158@cpc.com.tw>
2024-11-15 09:20:14 +0800

    try again with new json and new update python

--- update_mongodb.py
+++ update_mongodb.py
@@ -1,16 +1,24 @@
+import os
 import json
 from pymongo import MongoClient
-import os
 
-# 讀取 MongoDB URI
+# 從環境變數中讀取 MongoDB URI
 mongodb_uri = os.getenv("MONGODB_URI")
 client = MongoClient(mongodb_uri)
-db = client["sensor_data_db"]
-collection = db["temperature_data"]
 
-# 讀取 JSON 文件並插入數據
-with open("sales_data.json", "r") as file:
-    data = json.load(file)
-    for record in data:
-        collection.insert_one(record)
-        print("Inserted record:", record)
+try:
+    # 連接到資料庫並指定集合名稱
+    db = client["sensor_data_db"]
+    collection = db["temparature_data"]
+    print("Connected to Database")
+
+    # 讀取並插入 JSON 文件中的數據
+    with open("temparature_data.json", "r") as file:
+        data = json.load(file)
+        for record in data:
+            collection.insert_one(record)
+            print("Inserted record:", record)
+except Exception as e:
+    print("Database operation failed:", e)
+finally:
+    client.close()
