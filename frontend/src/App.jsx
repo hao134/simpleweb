0d09ddf4d9acd4d06e4ba436dcfd4bc951ff8bcf
hao134 <hsh319ab@gmail.com>
2024-11-25 12:33:04 +0800

    fix the n.map is not a function problem 2 => useState(All) to useState([All])

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,8 @@ const App = () => {
   const [data, setData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
   const [dateRange, setDateRange] = useState({ start: "", end: "" });
-  const [selectedWarehouse, setSelectedWarehouse] = useState("All");
+  //fix the problem n.map is not a function 2
+  const [selectedWarehouse, setSelectedWarehouse] = useState(["All"]);
   const [error, setError] = useState(null);
   
   // 當初次進入時獲取資料
@@ -25,12 +26,13 @@ const App = () => {
       const inDateRange =
         (!dateRange.start || date >= new Date(dateRange.start)) &&
         (!dateRange.end || date <= new Date(dateRange.end));
-      const matchesWarehouse = 
-        selectedWarehouse.includes("All") ||
-        selectedWarehouse.some((warehouse) => warehouse === item.location)
-      // const matchesWarehouse =
-      //   selectedWarehouse.includes("All") || 
-      //   selectedWarehouse.includes(item.location);
+      // //fix the problem n.map is not a function 1
+      // const matchesWarehouse = 
+      //   selectedWarehouse.includes("All") ||
+      //   selectedWarehouse.some((warehouse) => warehouse === item.location)
+      const matchesWarehouse =
+        selectedWarehouse.includes("All") || 
+        selectedWarehouse.includes(item.location);
       return inDateRange && matchesWarehouse;
     });
     setFilteredData(filtered)
